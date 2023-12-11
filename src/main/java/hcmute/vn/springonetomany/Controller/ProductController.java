7720dce458d49b2f2b52154e3170f3e063d8ce65
thaian1234 <121446197+thaian1234@users.noreply.github.com>
2023-12-11 09:55:25 +0700

    sua nhanh payment

--- src/main/java/hcmute/vn/springonetomany/Controller/ProductController.java
+++ src/main/java/hcmute/vn/springonetomany/Controller/ProductController.java
@@ -18,7 +18,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 
 @Controller
@@ -112,6 +115,16 @@ public class ProductController {
             if (!product.getRatings().isEmpty()) {
             	numberOfRating = product.getRatings().size();
                 ratingPoint = product.getRatingPoint();
+            }
+            Optional<Category> optionalCategory  = categoryService.getCategory(product.getCategory().getId());
+            if (optionalCategory.isPresent()) {
+                Category currentCategory = optionalCategory.get();
+                Set<Product> categoryProducts = currentCategory.getProducts();
+                List<Product> categoryProductList = new ArrayList<>(categoryProducts);
+                categoryProductList.remove(product);
+                int maxProductsCount = Math.min(4, categoryProductList.size());
+                List<Product> relevantProducts = categoryProductList.subList(0, maxProductsCount);
+                model.addAttribute("relevantProducts", relevantProducts);
             }
             model.addAttribute("listRating", listRating);
             model.addAttribute("ratingPoint", ratingPoint);
